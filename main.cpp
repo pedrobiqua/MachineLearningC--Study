ab52044f3001d62159af17260d004c53e8e139c5
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2023-04-03 15:25:35 -0300

    Add coments
    @pedrobiqua

--- main.cpp
+++ main.cpp
@@ -22,6 +22,12 @@ int main()
     const string file_path_fix = "iris_fix.csv";
     std::ifstream data_stream(file_path);
     std::string data_string((std::istreambuf_iterator<char>(data_stream)), std::istreambuf_iterator<char>());
+    /*
+    Padrão usado para substituir as strings por números
+    Iris-setosa -> 1
+    Iris-versicolor -> 2
+    Iris-virginica -> 3
+    */
     data_string = std::regex_replace(data_string, std::regex("Iris-setosa"), "1");
     data_string = std::regex_replace(data_string, std::regex("Iris-versicolor"), "2");
     data_string = std::regex_replace(data_string, std::regex("Iris-virginica"), "3");
